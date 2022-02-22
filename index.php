54d0bcfa728e434de87752855ba70621c74a8e20
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-02-22 11:56:12 +0100

    fini

--- index.php
+++ index.php
@@ -25,7 +25,68 @@
 
     // TODO Votre code ici, commencez par require un des objet de connexion que nous avons fait ensemble.
 
+    $server = 'localhost';
+    $db = 'exo_196';
+    $user = 'root';
+    $pass = '';
 
+    $bdd = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user,$pass);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    /* 3. Récupérez l'age minimum des utilisateurs.*/
+
+    $stmt = $bdd->prepare("SELECT MIN(age) as mini FROM user");
+
+    if ($stmt->execute()){
+      $min = $stmt->fetch();
+      echo "Le mini est " . $min['mini'] . "ans ! <br>";
+    }
+
+    /* 4. Récupérez l'âge maximum des utilisateurs.*/
+
+    $stmt = $bdd->prepare("SELECT MAX(age) as max FROM user");
+
+    if ($stmt->execute()){
+        $max = $stmt->fetch();
+        echo "Le max est " .$max['max'] . "ans ! <br>";
+    }
+
+    /* 5. Récupérez le nombre total d'utilisateurs dans la table à l'aide de la fonction d'agrégation COUNT().*/
+
+    $stmt = $bdd->prepare("SELECT count(*) as number FROM user");
+
+    if ($stmt->execute()){
+        $count = $stmt->fetch();
+        echo "Le total est " .$count['number']. " ! <br>";
+    }
+
+    /* 6. Récupérer le nombre d'utilisateurs ayant un numéro de rue plus grand ou égal à 5.*/
+
+    $stmt = $bdd->prepare("SELECT count(*) as num FROM user WHERE numero >= 5");
+
+    if ($stmt->execute()){
+        $count = $stmt->fetch();
+        echo "Le nombre est " .$count['num']. " ! <br>";
+    }
+
+    /* 7. Récupérez la moyenne d'âge des utilisateurs. */
+
+    $stmt = $bdd->prepare("SELECT AVG(age) as moyenne FROM user");
+
+    if ($stmt->execute()){
+        $avg = $stmt->fetch();
+        echo "La moyenne d'age est " .$avg['moyenne']. "ans ! <br>";
+    }
+
+    /* 8. Récupérer la somme des numéros de maison des utilisateurs ( bien que ca n'ait pas de sens ).*/
+
+    $stmt = $bdd->prepare("SELECT SUM(numero) as somme FROM user");
+
+    if ($stmt->execute()){
+        $sum = $stmt->fetch();
+        echo "La somme est " .$sum['somme']. " ! <br>";
+    }
     ?>
 </body>
 </html>
